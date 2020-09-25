c3189e6cbf997b831d5a3d78a38c988e2d1e415a
Miguel Chuil <chuildeveloper@gmail.com>
2020-09-25 17:19:37 -0500

    iniciando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EncuestasComponent } from './pages/encuestas/encuestas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    EncuestasComponent
   ],
   imports: [
     BrowserModule,
